ee239a88659411b0a652e41cc610dff1f89620b4
GJ <TALLESTTOWER1@GMAIL.COM>
2019-01-18 22:19:22 -0800

    attempted slide, failed

--- classes/player_class.lua
+++ classes/player_class.lua
@@ -2,6 +2,10 @@
 
 player = {}
 
+function player:reFix()
+  self.phys.fixture = love.physics.newFixture(self.phys.body, self.phys.shape, 5) -- A higher density gives it more mass.
+end
+
 function player:init(world)
 
   --[[
@@ -14,7 +18,8 @@ function player:init(world)
   -speed (fconfig)
   ]]
 
-
+  self.standshape = love.physics.newRectangleShape(0, 0, 50, 100)
+  self.slideshape = love.physics.newRectangleShape(0, 0, 50, 40)
   self.jumpcounter = 1
   self.jumpmax = 3
   self.state = "land"
@@ -23,8 +28,8 @@ function player:init(world)
   self.phys = {}
   self.phys.body = love.physics.newBody(world, 200, 550, "dynamic")
   self.phys.body:setFixedRotation(true)
-  self.phys.shape = love.physics.newRectangleShape(0, 0, 50, 100)
-  self.phys.fixture = love.physics.newFixture(self.phys.body, self.phys.shape, 5) -- A higher density gives it more mass.
+  self.phys.shape = self.standshape
+  self:reFix()
   self.phys.fixture:setFriction(.5)
 end
 
@@ -41,21 +46,39 @@ function player:update(dt)
     self.jumpcounter = 1
     self.dashed = false
     self.phys.body:setLinearDamping(0)
+  elseif self.state == "slide" then
+
   end
 
-  if love.keyboard.isDown("d") then --press the d key to push the player to the right
-    self.dir = 1
-    self.phys.body:applyForce(10000, 0)
-  elseif love.keyboard.isDown("a") then --press a key to push the player to the left
-    self.dir = -1
-    self.phys.body:applyForce(-10000, 0)
+  --[[if love.keyboard.isDown("lshift") then --SLIDE SHAPE
+    self.state = "slide"
+    --print("slide")
+    self.phys.shape = self.slideshape
+    self:reFix()
   end
+  function love.keyreleased(key)
+    if key == "lshift" then
+      self.state = "land"
+      self.phys.shape = self.standshape
+      self:reFix()
+    end
+  end]]
+
+  --if not self.state == "slide" then
+    if love.keyboard.isDown("d") then --press the d key to push the player to the right
+      self.dir = 1
+      self.phys.body:applyForce(10000, 0)
+    elseif love.keyboard.isDown("a") then --press a key to push the player to the left
+      self.dir = -1
+      self.phys.body:applyForce(-10000, 0)
+    end
+  --end
   xv,yv = self.phys.body:getLinearVelocity()
   function love.keypressed(key)
      if key == "escape" then
        love.event.quit()
      end
-     if key == "space" then --if jumping breaks the < might be it
+     if key == "space" then --AIR CONTROL
        if self.jumpcounter <= self.jumpmax and self.state ~= "dash" then --Jump
         self.jumpcounter = self.jumpcounter+1
         self.phys.body:setLinearVelocity(xv, -500)
@@ -66,7 +89,6 @@ function player:update(dt)
          self.dashed = true
        end
        if self.state == "dash" and self.dashed == true then --Cancel
-         print("cancel")
          self.phys.body:setLinearVelocity(0, -200)
          self.state = "air"
        end
